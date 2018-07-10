a0561b3c9708b3925dab46d2878d4c3759926942
Justin <you@example.com>
2018-07-10 15:40:26 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <avr/io.h>
+
+
+
+int main(void)
+{
+	DDRB=0b01111111; //Ganzer Port B als Ausgang definieren
+	PORTB=0x00; 
+
+	uint8_t test=0;
+	uint16_t x=0;
+	
+	while(1)
+	{ 
+	
+		if ( !(PINB & (1<<PINB7) )) 
+		{
+			test++;
+
+		}
+		
+		PORTB=test;	
+
+		//for(x=0;x<60000;x++);//zuerst auf 6000 zählen und dan +1
+	
+		
+	} //end while
+}//end of main
+
+
+
+
